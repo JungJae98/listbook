414b1aa26bb72b80f8f93a804c225ff7aef619d1
aabds <wogus8880@gmail.com>
2023-06-20 20:49:48 +0900

    작업완료

--- src/main/java/com/example/listbook/dto/ListbookDTO.java
+++ src/main/java/com/example/listbook/dto/ListbookDTO.java
@@ -1,2 +1,21 @@
-package com.example.listbook.dto;public class ListbookDTO {
+package com.example.listbook.dto;
+
+import com.example.listbook.entity.Listbook;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ListbookDTO {
+    private Long idx;
+    private String title;
+    private String author;
+    private String detail;
+    private LocalDateTime regDate, modDate;
 }
--- src/main/java/com/example/listbook/entity/Listbook.java
+++ src/main/java/com/example/listbook/entity/Listbook.java
@@ -1,2 +1,38 @@
-package com.example.listbook.entity;public class Listbook {
+package com.example.listbook.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Listbook extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 50, nullable = false)
+    private String author;
+
+    @Column(length = 1500, nullable = false)
+    private  String detail;
+
+    public void changeTitle(String title){
+        this.title = title;
+    }
+
+    public void changeAuthor(String author){
+        this.author = author;
+    }
+
+    public void changeDetail(String detail){
+        this.detail = detail;
+    }
+
 }
